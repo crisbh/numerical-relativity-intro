f5db1c722e0a655089d8737514a381951e2b4829
Cristian Barrera-Hinojosa <cristian.barrera@uv.cl>
2025-01-20 22:56:52 -0300

    update

---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/numerical-relativity-intro/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Slides",
+    "uri": "/numerical-relativity-intro/slides/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/numerical-relativity-intro/tags/index.html"
+  }
+]
